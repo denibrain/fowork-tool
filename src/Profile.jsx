82c8884e2829c089b6581dc5a8fb9df8a74e1e04
Denis R <rudev2@mxbud.com>
2019-02-03 15:09:56 +0500

    Profile data

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -1,11 +1,34 @@
 import React, { Component } from "react";
 
 class Profile extends Component {
+    state = {
+        profile: null,
+        error: ""
+    };
+
+    componentDidMount() {
+        this.loadUserProfile();
+    }
+
+    loadUserProfile() {
+        this.props.auth.getProfile((profile, error) => {
+            this.setState({ profile: profile, error:error });
+            console.log(error);
+        });
+    }
+
     render() {
+        const { profile } = this.state;
+        if (!profile) return null;
         return (
-            <div>
-                <h1>Profile</h1>
-            </div>
+            <>
+                <div>
+                    <h1>Profile</h1>
+                    <p>{profile.nickname}</p>
+                    <img alt="profile.pic" src={profile.picture} />
+                    <pre>{JSON.stringify(profile, null, 2)}</pre>
+                </div>
+            </>
         );
     }
 }
